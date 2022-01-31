58e6ee563067a3a436c913d0d9ea23d3d6b1d9ea
Daniel Solís <75152170+d4ndynoob@users.noreply.github.com>
2022-01-31 13:39:36 -0600

    First commit

---
+++ logica.js
@@ -0,0 +1,6 @@
+var link = document.getElementById('header__a');
+
+link.addEventListener('click', function(){
+    document.getElementById('header__sub-ul').classList.toggle('header__sub-ul--mostrar');
+
+});
